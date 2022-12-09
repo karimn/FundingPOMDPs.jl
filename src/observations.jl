3cff652aae73a56d549f29956fa190bc9e6c7a01
Karim Naguib <karimn2.0@gmail.com>
2022-12-09 14:42:45 -0500

    Remove unused parameter type

--- src/observations.jl
+++ src/observations.jl
@@ -6,34 +6,34 @@ end
 getprogramid(o::ProgramEvalObservation) = o.programid
 
 struct EvalObservation <: AbstractEvalObservation
-    po::Dict{Int64, ProgramEvalObservation}
+    programobs::Dict{Int64, AbstractProgramEvalObservation}
 end
 
-EvalObservation(pos::Vector{ProgramEvalObservation}) = EvalObservation(Dict(getprogramid(po) => po for po in pos))
+EvalObservation(pos::Vector{AbstractProgramEvalObservation}) = EvalObservation(Dict(getprogramid(po) => po for po in pos))
 
-struct StudySampleDistribution{T <: AbstractProgramState} <: AbstractSampleDistribution
-    programstate::T
+struct StudySampleDistribution <: AbstractStudySampleDistribution
+    programstate::AbstractProgramState
     samplesize::Int64
 end
 
 getprogramid(d::StudySampleDistribution) = getprogramid(d.programstate)
-Base.rand(rng::Random.AbstractRNG, d::StudySampleDistribution) = rand(rng, d.programstate, d.samplesize) 
+Base.rand(rng::Random.AbstractRNG, d::StudySampleDistribution) = Base.rand(rng, d.programstate, d.samplesize) 
 logpdf(d::StudySampleDistribution, ds::StudyDataset) = logpdf(d.programstate, ds)
-POMDPs.pdf(d::StudySampleDistribution, ds::StudyDataset) = pdf(d.programstate, ds)
+Distributions.pdf(d::StudySampleDistribution, ds::StudyDataset) = Distributions.pdf(d.programstate, ds)
 logpdf(d::StudySampleDistribution, o::ProgramEvalObservation) = logpdf(d, o.d)
-POMDPs.pdf(d::StudySampleDistribution, o::ProgramEvalObservation) = pdf(d, o.d)
+Distributions.pdf(d::StudySampleDistribution, o::ProgramEvalObservation) = Distributions.pdf(d, o.d)
 
-struct MultiStudySampleDistribution{T <: AbstractProgramState} <: AbstractSampleDistribution
-    sds::Dict{Int64, StudySampleDistribution{T}}
+struct MultiStudySampleDistribution <: AbstractSampleDistribution 
+    sds::Dict{Int64, AbstractStudySampleDistribution}
 end
 
-MultiStudySampleDistribution{T}(sds::Vector{StudySampleDistribution{T}}) where{T <: AbstractProgramState} = MultiStudySampleDistribution{T}(Dict(getprogramid(d) => d for d in sds))
-Base.rand(rng::Random.AbstractRNG, msd::MultiStudySampleDistribution{T}) where {T <: AbstractProgramState} = EvalObservation{T}([rand(rng, d) for d in msd.sds])
+MultiStudySampleDistribution(sds::Vector{AbstractStudySampleDistribution})= MultiStudySampleDistribution(Dict(getprogramid(d) => d for d in sds))
+Base.rand(rng::Random.AbstractRNG, msd::MultiStudySampleDistribution) = EvalObservation(Dict(pid => Base.rand(rng, d) for (pid, d) in msd.sds))
 
 function logpdf(msd::MultiStudySampleDistribution, o::EvalObservation) 
-    length(msd.sds) == length(o.po) || throw(ArgumentError("mismatch in number of samples and distributions"))
+    length(msd.sds) == length(o.programobs) || throw(ArgumentError("mismatch in number of samples and distributions"))
 
-    return sum([logpdf(msd.sds[program_id], po) for (programid, po) in o])
+    return sum([logpdf(msd.sds[programid], po) for (programid, po) in o.programobs])
 end
 
-POMDPs.pdf(msd::MultiStudySampleDistribution, o::EvalObservation) = exp(logpdf(msd, o)) 
+Distributions.pdf(msd::MultiStudySampleDistribution, o::AbstractEvalObservation) = exp(logpdf(msd, o)) 
