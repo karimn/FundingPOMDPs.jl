13895b81ff90a86de1d646641c36f141eb1bb90d
Karim Naguib <karimn2.0@gmail.com>
2022-12-13 09:34:34 -0500

    Add length

--- src/actions.jl
+++ src/actions.jl
@@ -4,7 +4,9 @@ end
 
 ImplementEvalAction(pids::Vector{Int64}) = ImplementEvalAction(BitSet(pids))
 
-implements(a::ImplementEvalAction, i::Int64) = in(a.implement_eval_programs, i)
+implements(a::ImplementEvalAction, i::Int64) = in(i, a.implement_eval_programs)
 get_evaluated_program_ids(a::ImplementEvalAction) = a.implement_eval_programs
 
 Base.show(io::IO, a::ImplementEvalAction) = print(io, "ImplementEvalAction([$(a.implement_eval_programs)])")  
+
+Base.length(as::AbstractActionSet) = numactions(as)
