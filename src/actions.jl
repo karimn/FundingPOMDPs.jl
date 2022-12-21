742b343f7ab97d2a2f0a6c6141671498a0de91b7
Karim Naguib <karimn2.0@gmail.com>
2022-12-21 15:16:57 -0500

    implement only action

--- src/actions.jl
+++ src/actions.jl
@@ -9,4 +9,15 @@ get_evaluated_program_ids(a::ImplementEvalAction) = a.implement_eval_programs
 
 Base.show(io::IO, a::ImplementEvalAction) = print(io, "ImplementEvalAction([$(a.implement_eval_programs)])")  
 
+@with_kw struct ImplementOnlyAction <: AbstractFundingAction
+    implement_programs::BitSet = BitSet() 
+end
+
+ImplementOnlyAction(pids::Vector{Int64}) = ImplementOnlyAction(BitSet(pids))
+
+implements(a::ImplementOnlyAction, i::Int64) = in(i, a.implement_programs)
+get_evaluated_program_ids(::ImplementOnlyAction) = BitSet() 
+
+Base.show(io::IO, a::ImplementOnlyAction) = print(io, "ImplementOnlyAction([$(a.implement_programs)])")  
+
 Base.length(as::AbstractActionSet) = numactions(as)
