acdc7ef5706e52fcf7a6d81c9d5d172543cd8639
Karim Naguib <karimn2.0@gmail.com>
2022-12-21 15:18:13 -0500

    Can change action type + minor renames of variables

--- src/kbandit.jl
+++ src/kbandit.jl
@@ -1,14 +1,14 @@
 
-struct KBanditActionSet <: AbstractActionSet
-    actions::Vector{ImplementEvalAction}
+struct KBanditActionSet{A} <: AbstractActionSet where A <: AbstractFundingAction
+    actions::Vector{A}
 end
 
-function KBanditActionSet(nprograms, nimplement) 
+function KBanditActionSet{A}(nprograms, nimplement) where A <: AbstractFundingAction
     actionlist = map(Combinatorics.combinations(1:nprograms, nimplement)) do programidx
-        ImplementEvalAction(programidx)
+        A(programidx)
     end
 
-    push!(actionlist, ImplementEvalAction()) # Add a do-nothing action
+    push!(actionlist, A()) # Add a do-nothing action
 
     return KBanditActionSet(actionlist)
 end
@@ -35,34 +35,32 @@ struct KBanditFundingMDP{A <: AbstractFundingAction, R <: AbstractRewardModel} <
     rng::Random.AbstractRNG
     actionset::AbstractActionSet
 
-    initstate::CausalState
+    curr_state::CausalState
 end
 
 function KBanditFundingMDP{A, R}(r::R, d::Float64, ni::Int64, ss::Int64, hyperparam::Hyperparam, dgp::AbstractDGP, rng::Random.AbstractRNG = Random.GLOBAL_RNG) where {A, R <: AbstractRewardModel}
     ni <= numprograms(dgp) || throw(ArgumentError("number of programs to implement greater than number of programs"))
 
-    initstate = Base.rand(rng, dgp)
+    curr_state = Base.rand(rng, dgp)
 
-    return KBanditFundingMDP{A, R}(r, d, ni, ss, hyperparam, dgp, rng, KBanditActionSet(numprograms(dgp), ni), initstate)
+    return KBanditFundingMDP{A, R}(r, d, ni, ss, hyperparam, dgp, rng, KBanditActionSet{A}(numprograms(dgp), ni), curr_state)
 end 
 
 mdp(m::KBanditFundingMDP) = m 
 
 struct KBanditFundingPOMDP{A <: AbstractFundingAction, R <: AbstractRewardModel, B <: AbstractBelief} <: POMDP{CausalState, A, EvalObservation}
     mdp::KBanditFundingMDP{A, R}
-
-    data::Vector{Vector{StudyDataset}}
-    initbelief::B
+    curr_belief::B
 end
 
-function KBanditFundingPOMDP{A, R, B}(mdp::KBanditFundingMDP{A, R}, data::Vector{Vector{StudyDataset}}) where {A, R <: AbstractRewardModel, B <: AbstractBelief} 
-    return KBanditFundingPOMDP{A, R, B}(mdp, data, B(data, (mdp.hyperparam)))
+function KBanditFundingPOMDP{A, R, B}(mdp::KBanditFundingMDP{A, R}, data::Vector{Vector{StudyDataset}}) where {A <: AbstractFundingAction, R <: AbstractRewardModel, B <: AbstractBelief} 
+    return KBanditFundingPOMDP{A, R, B}(mdp, B(data, mdp.hyperparam))
 end
 
-function KBanditFundingPOMDP{A, R, B}(mdp::KBanditFundingMDP{A, R}) where {A, R <: AbstractRewardModel, B <: AbstractBelief}
-    initdatasets = Vector{Vector{StudyDataset}}(undef, numprograms(mdp.initstate))
+function KBanditFundingPOMDP{A, R, B}(mdp::KBanditFundingMDP{A, R}) where {A <: AbstractFundingAction, R <: AbstractRewardModel, B <: AbstractBelief}
+    initdatasets = Vector{Vector{StudyDataset}}(undef, numprograms(mdp.curr_state))
 
-    for (pid, ds) in getdatasets(Base.rand(mdp.rng, mdp.initstate, mdp.studysamplesize))
+    for (pid, ds) in getdatasets(Base.rand(mdp.rng, mdp.curr_state, mdp.studysamplesize))
         initdatasets[pid] = [ds] 
     end
     
@@ -91,6 +89,10 @@ hyperparam(m::KBanditFundingProblem) = mdp(m).hyperparam
 
 numprograms(m::KBanditFundingProblem) = numprograms(mdp(m).dgp)
 
+rewardmodel(m::KBanditFundingProblem) = mdp(m).rewardmodel
+
+initialbelief(m::KBanditFundingPOMDP) = m.curr_belief 
+
 POMDPs.discount(m::KBanditFundingProblem) = mdp(m).discount
 
 POMDPs.isterminal(m::KBanditFundingProblem) = false 
@@ -104,9 +106,7 @@ POMDPs.actions(m::KBanditFundingProblem, ::Any) = POMDPs.actions(m)
 
 POMDPs.reward(m::KBanditFundingProblem{A, R, B}, s::CausalState, a::A) where {A, R, B} = expectedutility(rewardmodel(m), s, a)
 
-rewardmodel(m::KBanditFundingProblem) = mdp(m).rewardmodel
-
-POMDPs.initialstate(m::KBanditFundingMDP) = POMDPTools.Deterministic(m.initstate) # CausalStateDistribution(mdp(m).dgp) 
+POMDPs.initialstate(m::KBanditFundingMDP) = POMDPTools.Deterministic(m.curr_state) # CausalStateDistribution(mdp(m).dgp) 
 POMDPs.initialstate(m::KBanditFundingPOMDP) = initialbelief(m) # CausalStateDistribution(mdp(m).dgp) 
 
 function POMDPs.observation(pomdp::KBanditFundingPOMDP{A, R, B}, s::CausalState, a::A, sp::CausalState) where {A, R, B}
@@ -118,5 +118,3 @@ function POMDPs.observation(pomdp::ProgramBanditWrapper{A, R, B}, s::ProgramCaus
 
     return StudySampleDistribution(s, pomdp.wrapped_problem.mdp.studysamplesize)
 end
-
-initialbelief(m::KBanditFundingPOMDP) = m.initbelief 
