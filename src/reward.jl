404059a9f9de8adab335d8b85e6e3b901b9e3004
Karim Naguib <karimn2.0@gmail.com>
2023-01-06 16:01:03 -0500

    Remove unnecessary function

--- src/reward.jl
+++ src/reward.jl
@@ -3,5 +3,5 @@ struct ExponentialUtilityModel <: AbstractRewardModel
 end
 
 utility(m::ExponentialUtilityModel, outcome) = 1 - exp(- m.α * outcome)
+
 expectedutility(m::ExponentialUtilityModel, μ::Float64, σ::Float64) = 1 - exp(- m.α * μ + m.α^2 * σ^2 / 2)
-expectedutility(m::ExponentialUtilityModel, μ::AbstractVector, σ::AbstractVector) = 1 .- exp.(- m.α .* μ + m.α^2 .* σ.^2 / 2)
