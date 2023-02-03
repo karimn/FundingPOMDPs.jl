a79685a2690687acdf75695bb1bf8aa58b7420a4
Karim Naguib <karimn2.0@gmail.com>
2023-02-03 11:07:20 -0500

    Adding random policy sim

--- src/testpftdpw.jl
+++ src/testpftdpw.jl
@@ -2,7 +2,7 @@ doc = """
 Funding POMDP simulation.
 
 Usage:
-    testpftdpw.jl <greedy file> <planned file> [options] [--risk-neutral | --alpha=<alpha>]
+    testpftdpw.jl <greedy file> <planned file> <random file> [options] [--risk-neutral | --alpha=<alpha>]
 
 Options:
     --append, -a                               Append data
@@ -13,16 +13,19 @@ Options:
     --depth=<depth>, -d <depth>                Planning depth [default: 10]
     --alpha=<alpha>                            Exponential utility function alpha parameter [default: 0.25]
     --risk-neutral                             Risk neutral utility
-    --plan-algo=<algo>                         Planning algorithm (pftdpw, random) [default: pftdpw]
-    --pftdpw-iter=<iter>                       Number of DPW solver iterations [default: 50]
+    --pftdpw-iter=<iter>                       Number of DPW solver iterations [default: 100]
     --save-pftdpw-tree                         Save MCTS tree in action info
+    --k-state=<k>                              State hyperparameter k [default: 4.5]
+    --reward-only                              Return rewards only in simulation data
 """
+
+# --plan-algo=<algo>                         Planning algorithm (pftdpw, random) [default: pftdpw]
 
 import DocOpt
 
 args = DocOpt.docopt(
     doc, 
-    isinteractive() ? "greedy_sim_test.jls pftdpw_sim_test.jls -p 5 -s 2 -t 3 --numprocs=1 --plan-algo=random" : ARGS, 
+    isinteractive() ? "../temp-data/greedy_sim_test.jls ../temp-data/pftdpw_sim_test.jls ../temp-data/random_sim_test.jls -p 2 -s 2 -t 2 --numprocs=2 --pftdpw-iter=2" : ARGS, 
     version = v"1"
 )
 
@@ -89,7 +92,7 @@ pftdpw_solver = MCTS.DPWSolver(
     exploration_constant = 25.0,
     n_iterations = parse(Int, args["--pftdpw-iter"]), #20,  #100,
     enable_action_pw = false,  
-    k_state = 4.5,
+    k_state = parse(Float64, args["--k-state"]), # 4.5,
     alpha_state = 1/10.0,
     check_repeat_state = false,
     estimate_value = MCTS.RolloutEstimator(random_solver),
@@ -106,6 +109,7 @@ bayes_updater = FullBayesianUpdater(RNG, bayes_model)
 
 planned_sims = Vector{POMDPTools.Sim}(undef, NUM_SIM)
 greedy_sims = Vector{POMDPTools.Sim}(undef, NUM_SIM)
+random_sims = Vector{POMDPTools.Sim}(undef, NUM_SIM)
 
 pm = ProgressMeter.Progress(NUM_SIM, desc = "Preparing sims...")
 
@@ -152,6 +156,11 @@ pm = ProgressMeter.Progress(NUM_SIM, desc = "Preparing sims...")
     greedy_sim_rng = Random.MersenneTwister()
     planned_sim_rng = copy(greedy_sim_rng)
 
+    greedy_sims[sim_index] = POMDPSimulators.Sim(greedy_pomdp, greedy_policy, bayes_updater, initialbelief(greedy_pomdp), init_s, rng = greedy_sim_rng, max_steps = NUM_SIM_STEPS)
+    planned_sims[sim_index] = POMDPSimulators.Sim(planned_pomdp, pftdpw_planner, particle_updater, bayes_b, init_s, rng = planned_sim_rng, max_steps = NUM_SIM_STEPS)
+    random_sims[sim_index] = POMDPSimulators.Sim(planned_pomdp, random_planner, bayes_updater, bayes_b, init_s, rng = planned_sim_rng, max_steps = NUM_SIM_STEPS)
+
+    #=
     if args["--plan-algo"] == "pftdpw"
         planned_sims[sim_index] = POMDPSimulators.Sim(planned_pomdp, pftdpw_planner, particle_updater, bayes_b, init_s, rng = planned_sim_rng, max_steps = NUM_SIM_STEPS)
     elseif args["--plan-algo"] == "random"
@@ -159,51 +168,69 @@ pm = ProgressMeter.Progress(NUM_SIM, desc = "Preparing sims...")
     else
         error("Unknown planning algorithm '$(args["--plan-algo"])'")
     end
-
-    greedy_sims[sim_index] = POMDPSimulators.Sim(greedy_pomdp, greedy_policy, bayes_updater, initialbelief(greedy_pomdp), init_s, rng = greedy_sim_rng, max_steps = NUM_SIM_STEPS)
+    =#
 
     ProgressMeter.next!(pm)
 end
 
-@everywhere function get_sim_data(sim::POMDPTools.Sim, hist::POMDPTools.SimHistory)
-    actions = collect(POMDPSimulators.action_hist(hist)) 
-    beliefs = collect(POMDPSimulators.belief_hist(hist))
-    trees = [ s_ainfo !== nothing && haskey(s_ainfo, :tree) ? s_ainfo[:tree] : missing for s_ainfo in POMDPSimulators.ainfo_hist(hist) ]
+function create_sim_data_getter(actual_reward_only = false)
+    function inner_get_sim_data(sim::POMDPTools.Sim, hist::POMDPTools.SimHistory)
+        sim_data = (actual_reward = collect(POMDPSimulators.reward_hist(hist)),)
 
-    return (
-        action = actions, 
-        actual_reward = collect(POMDPSimulators.reward_hist(hist)),
-        belief = beliefs,
-        expected_reward = [expectedutility(rewardmodel(sim.pomdp), b, a) for (b, a) in zip(beliefs, actions)],
-        state = collect(POMDPSimulators.state_hist(hist)),
-        tree = trees,
+        if !actual_reward_only
+            actions = collect(POMDPSimulators.action_hist(hist)) 
+            beliefs = collect(POMDPSimulators.belief_hist(hist))
+            trees = [ s_ainfo !== nothing && haskey(s_ainfo, :tree) ? s_ainfo[:tree] : missing for s_ainfo in POMDPSimulators.ainfo_hist(hist) ]
 
-        total_undiscounted_actual_reward = POMDPSimulators.undiscounted_reward(hist)
-    )
+            sim_data = (
+                action = actions, 
+                sim_data...,
+                belief = beliefs,
+                expected_reward = [expectedutility(rewardmodel(sim.pomdp), b, a) for (b, a) in zip(beliefs, actions)],
+                state = collect(POMDPSimulators.state_hist(hist)),
+                tree = trees,
+
+                total_undiscounted_actual_reward = POMDPSimulators.undiscounted_reward(hist)
+            )
+        end
+
+        return sim_data
+    end
+
+    return inner_get_sim_data
 end
 
 run_fun = NUM_PROCS > 1 ? POMDPTools.run_parallel : POMDPTools.run
 
+get_sim_data = create_sim_data_getter(args["--reward-only"])
+
 greedy_sim_data = run_fun(get_sim_data, greedy_sims; show_progress = true)
 planned_sim_data = run_fun(get_sim_data, planned_sims; show_progress = true) 
+random_sim_data = run_fun(get_sim_data, random_sims; show_progress = true) 
 
 if args["--append"]
     try
-        global greedy_sim_data = vcat(Serialization.deserialize(args["<greedy file>"]), greedy_sim_data)
+        global greedy_sim_data = vcat(Serialization.deserialize(args["<greedy file>"]), greedy_sim_data; cols = :union)
     catch 
         @warn "Output file doesn't exist -- creating new file" file = args["<greedy file>"]
     end
 
     try
-        global planned_sim_data = vcat(Serialization.deserialize(args["<planned file>"]), planned_sim_data)
+        global planned_sim_data = vcat(Serialization.deserialize(args["<planned file>"]), planned_sim_data; cols = :union)
     catch 
         @warn "Output file doesn't exist -- creating new file" file = args["<planned file>"]
     end
 
+    try
+        global random_sim_data = vcat(Serialization.deserialize(args["<random file>"]), random_sim_data; cols = :union)
+    catch 
+        @warn "Output file doesn't exist -- creating new file" file = args["<random file>"]
+    end
 end
 
 Serialization.serialize(args["<greedy file>"], greedy_sim_data)
 Serialization.serialize(args["<planned file>"], planned_sim_data)
+Serialization.serialize(args["<random file>"], random_sim_data)
 
 #x = Serialization.deserialize("greedy_sim_test.jls")
 #pftdpw_sim_data = Serialization.deserialize("pftdpw_sim.jls")
